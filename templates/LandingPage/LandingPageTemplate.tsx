5c74f00dd07e6ec4bb7fec8d4570cce48deed31b
Zefri <68502472+zefrifahlevi@users.noreply.github.com>
2023-06-23 17:11:41 +0700

    Update Data#3

--- templates/LandingPage/LandingPageTemplate.tsx
+++ templates/LandingPage/LandingPageTemplate.tsx
@@ -88,18 +88,17 @@ const LandingPageTemplate = () => {
               🎉
             </H1>
             <Subtitle>
-              Get started by us <InlineCode>Team Developer</InlineCode>
+              Development by us <InlineCode>Team Developer</InlineCode>
             </Subtitle>
           </Container>
         </Section>
         <Section>
           <Container className={styles.textContainer}>
-            <H2>This Is Us</H2>
+            <H2>About Us</H2>
             <Paragraph>
-              We are a student at Garut Institute Of Technoloy Computer Science few final steps that we were not able to perform
-              automatically. We have provided a complete list for you in the{" "}
-              <InlineCode>README.md</InlineCode> file. You should take care of
-              these before you can start developing your project.
+            Our group is a group of students who are at the {" "}
+            <InlineCode>Garut Institute of Technology</InlineCode>, with a focus on studying in the field of Computer Science. We consist of a group of individuals who are passionate and dedicated to the development of technology and computer science.
+            We have the same goal, which is to increase our understanding in the field of computer science and apply it in useful projects. We are always trying to improve our skills in programming, data analysis, artificial intelligence, computer networking and other relevant fields.
             </Paragraph>
           </Container>
         </Section>
@@ -107,9 +106,14 @@ const LandingPageTemplate = () => {
           <Container className={styles.technologyGridIntro}>
             <H2>Technologies</H2>
             <Paragraph>
-              Below you will find an overview of your chosen technologies
-              providing you helpful links and simple usage examples to get you
-              started.
+            In the campus environment, we apply and develop our knowledge in the {" "}
+            <InlineCode> C++</InlineCode> programming language. We understand the basics of programming with C++ and we are skilled in using concepts such as variables, control structures, functions, and data processing.
+            Apart from that, we also explore our skills in the {" "}
+            <InlineCode> Python</InlineCode>Python programming language. We have an understanding and use of existing data structures, functions and modules.
+            </Paragraph>
+            <Paragraph>
+            In developing with {" "} <InlineCode> HTML, CSS, JavaScript, React and Next.js</InlineCode>, we focus on developing responsive, interactive and easy-to-use web applications. We strive to use best practices in front-end development, including quality testing, efficient state setting and optimal performance.
+            We love to expand our knowledge in this modern web technology and keep learning about the latest trends and tools relevant to front-end development.
             </Paragraph>
           </Container>
           <Container wide>
--- templates/LandingPage/technologies.ts
+++ templates/LandingPage/technologies.ts
@@ -10,12 +10,8 @@ export const technologies: Technology[] = [
   {
     name: "Zefri Fahlevi Irdiansyah",
     description:
-      "Hello there, my full name is Zefri Fahlevi Irdiansyah, and people who's know me call me Zefri. For now, I live in Garut, West Java and I am a student majoring in Informatics Engineering, Garut Institut of Technology (ITG). I Love make something that useful for the others like make an application website and I'm prefer in Frontend website development using ReactJS or Next Js, and until now, I'm still learning, because everyday the technologies is continues to develop.",
+      "Hello there, my full name is Zefri Fahlevi Irdiansyah, and people who's know me call me Zefri. For now, I live in Garut, West Java and I am a student majoring in Informatics Engineering, Garut Institut of Technology (ITG). I Love make something that useful for the others like make an application website and I'm prefer in Frontend website development, and until now, I'm still learning, because everyday the technologies is continues to develop.",
     links: [
-      {
-        title: "Website",
-        url: "https://zefrifahlevi1.vercel.app/",
-      },
       {
         title: "Instagram",
         url: "https://www.instagram.com/zefrifahlevi/",
@@ -36,15 +32,11 @@ export const technologies: Technology[] = [
       "Hi, Nama saya adalah Ahmad nur sahid bisa di panggil Ahmad, lahir di Garut pada tanggal 05 Agustus. Saya adalah anak ke-2 dari 3 bersaudara. Sejak kecil, saya sudah tertarik dengan teknologi dan komputer, dan itu menjadi alasan utama saya memilih jurusan Teknik Informatika di ITG (Institut Teknologi Garut). Saat ini, saya sedang menempuh semester 2 dan masih terus belajar mengenai berbagai aspek teknologi informasi seperti pemrograman, Algoritma, dan pengolahan data. Walaupun kadang-kadang terasa sulit, namun saya merasa sangat tertantang dan semangat untuk terus belajar dan meningkatkan kemampuan saya di bidang ini.",
     links: [
       {
-        title: "Website",
-        url: "https://ahmaadn.my.id/",
-      },
-      {
         title: "Instagram",
         url: "https://www.instagram.com/a.ahmaadn",
       },
       {
-        title: "Github",
+        title: "GitHub",
         url: "https://github.com/ahmaadn",
       },
     ],
@@ -52,7 +44,7 @@ export const technologies: Technology[] = [
   {
     name: "Yoga Agustiansyah",
     description:
-      "Textnya",
+      "**Development**",
     links: [
       {
         title: "Instagram",
@@ -67,7 +59,7 @@ export const technologies: Technology[] = [
   {
     name: "Muhammad Heikal Hikmah",
     description:
-      "Textnya",
+      " Izin memperkenalkan diri, nama saya Muhammad Heikal Hikmah asal dari Garut sedang melakukan studi kuliah di ITG terimakasih atas perhatiannya",
     links: [
 
       {
@@ -87,7 +79,7 @@ export const technologies: Technology[] = [
   {
     name: "Dhika Restu Fauzi",
     description:
-      "Textnya",
+      "Perkenalkan saya Dhika panggil saja Dika",
     links: [
       {
         title: "Instagram",
@@ -102,38 +94,26 @@ export const technologies: Technology[] = [
   {
     name: "R Muhammad Arya Fajar Sastradiraja",
     description:
-      "Textnya",
+      "**Development**",
     links: [
       {
         title: "Instagram",
-        url: "/",
-      },
-      {
-        title: "Twitter",
-        url: "/",
+        url: "https://www.instagram.com/aaaaarya_03",
       },
       {
         title: "GitHub",
-        url: "/",
+        url: "https://github.com/Akyra001",
       },
     ],
   },
   {
     name: "Muhammad Rizki",
     description:
-      "Textnya",
+      "**Development**",
     links: [
       {
         title: "Instagram",
-        url: "/",
-      },
-      {
-        title: "Twitter",
-        url: "/",
-      },
-      {
-        title: "GitHub",
-        url: "/",
+        url: "https://www.instagram.com/mhmdikiii/",
       },
     ],
   },
