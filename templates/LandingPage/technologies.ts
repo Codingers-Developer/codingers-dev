1afc866ccdd3d089aa89871d000f81534518d98d
Zefri <68502472+zefrifahlevi@users.noreply.github.com>
2023-06-23 18:40:27 +0700

    Update Data#5

--- templates/LandingPage/technologies.ts
+++ templates/LandingPage/technologies.ts
@@ -10,7 +10,7 @@ export const technologies: Technology[] = [
   {
     name: "Zefri Fahlevi Irdiansyah",
     description:
-      "Hello there, my full name is Zefri Fahlevi Irdiansyah, and people who's know me call me Zefri. For now, I live in Garut, West Java and I am a student majoring in Informatics Engineering, Garut Institut of Technology (ITG). I Love make something that useful for the others like make an application website and I'm prefer in Frontend website development, and until now, I'm still learning, because everyday the technologies is continues to develop.",
+      "Hello there, my full name is Zefri Fahlevi Irdiansyah, and people who's know me call me Zefri. For now, I live in Garut, West Java and I am a student majoring in Informatics Engineering, Garut Institut of Technology (ITG). I Love make something that useful for the others like make an application website and I'm prefer in Frontend website development using ReactJS or Next Js, and until now, I'm still learning, because everyday the technologies is continues to develop.",
     links: [
       {
         title: "Instagram",
@@ -27,21 +27,6 @@ export const technologies: Technology[] = [
     ],
   },
   {
-    name: "Ahmad Nur Sahid",
-    description:
-      "Hi, Nama saya adalah Ahmad nur sahid bisa di panggil Ahmad, lahir di Garut pada tanggal 05 Agustus. Saya adalah anak ke-2 dari 3 bersaudara. Sejak kecil, saya sudah tertarik dengan teknologi dan komputer, dan itu menjadi alasan utama saya memilih jurusan Teknik Informatika di ITG (Institut Teknologi Garut). Saat ini, saya sedang menempuh semester 2 dan masih terus belajar mengenai berbagai aspek teknologi informasi seperti pemrograman, Algoritma, dan pengolahan data. Walaupun kadang-kadang terasa sulit, namun saya merasa sangat tertantang dan semangat untuk terus belajar dan meningkatkan kemampuan saya di bidang ini.",
-    links: [
-      {
-        title: "Instagram",
-        url: "https://www.instagram.com/a.ahmaadn",
-      },
-      {
-        title: "GitHub",
-        url: "https://github.com/ahmaadn",
-      },
-    ],
-  },
-  {
     name: "Yoga Agustiansyah",
     description:
       "Saya adalah Yoga Agustiansyah, seorang pria yang sangat antusias dalam dunia pemrograman. Saya memiliki pengalaman luas dalam beberapa bahasa pemrograman seperti JavaScript, TypeScript, Dart, Go, dan Python. Keahlian saya terutama berfokus pada pengembangan aplikasi seluler, di mana saya telah berhasil menggunakan Flutter dan React Native untuk menciptakan aplikasi yang menakjubkan. Saya juga memiliki keahlian dalam pengembangan web front-end menggunakan React JS. Saat ini, saya sedang dalam proses belajar yang intensif untuk memperdalam pengetahuan saya tentang teknologi seperti React Native, Flutter, React, Node JS, Golang, Python, HTML 5, dan CSS. Saya sangat bersemangat untuk terus mengembangkan keterampilan saya dalam pengembangan aplikasi seluler dan web front-end, dan saya juga memiliki ketertarikan yang kuat dalam mempelajari dan mengembangkan keahlian dalam pengembangan back-end.",
@@ -53,6 +38,21 @@ export const technologies: Technology[] = [
       {
         title: "GitHub",
         url: "https://github.com/yoga220802",
+      },
+    ],
+  },
+  {
+    name: "Ahmad Nur Sahid",
+    description:
+      "Hi, Nama saya adalah Ahmad nur sahid bisa di panggil Ahmad, lahir di Garut pada tanggal 05 Agustus. Saya adalah anak ke-2 dari 3 bersaudara. Sejak kecil, saya sudah tertarik dengan teknologi dan komputer, dan itu menjadi alasan utama saya memilih jurusan Teknik Informatika di ITG (Institut Teknologi Garut). Saat ini, saya sedang menempuh semester 2 dan masih terus belajar mengenai berbagai aspek teknologi informasi seperti pemrograman, Algoritma, dan pengolahan data. Walaupun kadang-kadang terasa sulit, namun saya merasa sangat tertantang dan semangat untuk terus belajar dan meningkatkan kemampuan saya di bidang ini.",
+    links: [
+      {
+        title: "Instagram",
+        url: "https://www.instagram.com/a.ahmaadn",
+      },
+      {
+        title: "GitHub",
+        url: "https://github.com/ahmaadn",
       },
     ],
   },
