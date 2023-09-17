db1dad1c2a12547f49332e0deafd74b19b8e9f95
Zefri <68502472+zefrifahlevi@users.noreply.github.com>
2023-09-17 19:28:46 +0700

    chore(technologies): updating description in ahmad

--- templates/LandingPage/technologies.ts
+++ templates/LandingPage/technologies.ts
@@ -44,7 +44,7 @@ export const technologies: Technology[] = [
   {
     name: "Ahmad Nur Sahid",
     description:
-      "Hi, Nama saya adalah Ahmad nur sahid bisa di panggil Ahmad, lahir di Garut pada tanggal 05 Agustus. Saya adalah anak ke-2 dari 3 bersaudara. Sejak kecil, saya sudah tertarik dengan teknologi dan komputer, dan itu menjadi alasan utama saya memilih jurusan Teknik Informatika di ITG (Institut Teknologi Garut). Saat ini, saya sedang menempuh semester 2 dan masih terus belajar mengenai berbagai aspek teknologi informasi seperti pemrograman, Algoritma, dan pengolahan data. Walaupun kadang-kadang terasa sulit, namun saya merasa sangat tertantang dan semangat untuk terus belajar dan meningkatkan kemampuan saya di bidang ini.",
+      "Hi, Nama saya adalah Ahmad nur sahid bisa di panggil Ahmad, lahir di Garut pada tanggal 05 Agustus. Saya adalah anak ke-2 dari 3 bersaudara. Sejak kecil, saya sudah tertarik dengan teknologi dan komputer, dan itu menjadi alasan utama saya memilih jurusan Teknik Informatika di ITG (Institut Teknologi Garut). Saat ini, saya sedang menempuh semester 3 dan masih terus belajar mengenai berbagai aspek teknologi informasi seperti pemrograman, Algoritma, dan pengolahan data. Walaupun kadang-kadang terasa sulit, namun saya merasa sangat tertantang dan semangat untuk terus belajar dan meningkatkan kemampuan saya di bidang ini.",
     links: [
       {
         title: "Instagram",
